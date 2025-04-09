342d45a79730ba0c7d1dbabf2ff2c70a55562572
sadoyin <sadoyin74@gmail.com>
2025-04-09 13:44:30 -0700

    es

--- src/components/service/FeatureSection.tsx
+++ src/components/service/FeatureSection.tsx
@@ -7,13 +7,13 @@ const FeatureSection = () => {
       <div className="w-full bg-[url(/features-bg.png)] h-full mt-[90px] py-[90px] px-0  bg-cover bg-center overflow-hidden">
         <div className="container pr-[15px] pl-[15px] mr-auto ml-auto">
           <Script src="https://widgets.coingecko.com/gecko-coin-market-ticker-list-widget.js" />
-          <gecko-coin-market-ticker-list-widget
+          {/* <gecko-coin-market-ticker-list-widget
             locale="en"
             dark-mode="true"
             outlined="true"
             initial-currency="usd"
             vce-ready=""
-          ></gecko-coin-market-ticker-list-widget>
+          ></gecko-coin-market-ticker-list-widget> */}
         </div>
       </div>
       <div className="container pr-[15px] pl-[15px] mr-auto ml-auto mb-6">
