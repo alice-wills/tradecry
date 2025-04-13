e0d8536dfe915af518156b6e003b503701b20ea6
sadoyin <sadoyin74@gmail.com>
2025-04-13 12:12:22 -0700

    marquee element

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,7 @@ import { UserButton, useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import { redirect } from "next/navigation";
 import HomeCoinGecko from "@/components/HomeCoinGecko";
+import MarqueeElement from "@/components/MarqueeElement";
 
 interface UserPublicMetadata {
   deposit?: string; // Add other properties you expect in publicMetadata
@@ -213,6 +214,7 @@ const HomePage = () => {
         <main className="p-6 sm:p-10 space-y-6">
           <div className="flex flex-col space-y-6 md:space-y-0 md:flex-row justify-between">
             <div className="mr-6">
+              <MarqueeElement/>
               <h1 className="text-4xl font-semibold mb-2">Dashboard</h1>
               <h2 className="text-gray-600 ml-0.5">
                 Welcome <strong>{user.firstName}</strong>
---
+++ src/components/MarqueeElement.tsx
@@ -0,0 +1,12 @@
+// components/TopMarquee.tsx
+
+export default function MarqueeElement() {
+    return (
+      <div className="w-full bg-blue-600 text-white overflow-hidden whitespace-nowrap py-2">
+        <div className="animate-marquee inline-block px-4">
+        Increased investment typically leads to earlier withdrawals
+        </div>
+      </div>
+    );
+  }
+  
